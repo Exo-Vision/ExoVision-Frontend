e2bf50060e29399f7ba43125c31a1b13527c3849
Lihan <lihan3k@gmail.com>
2025-10-04 09:26:24 +0900

    Create, Modify: pages 폴더 위치 변경, App.js Router 설정

--- exovision/src/App.js
+++ exovision/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
