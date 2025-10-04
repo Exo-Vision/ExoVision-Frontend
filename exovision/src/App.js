be4188bd3544161497467ae502c66c306aca7c0c
Lihan <lihan3k@gmail.com>
2025-10-04 22:49:33 +0900

    Feat: 메인페이지 라우터 추가

--- exovision/src/App.js
+++ exovision/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-
+import MainPage from './pages/'
 import Beginner from './pages/Beginner'
 import Result from './pages/Result'
 
@@ -9,6 +9,7 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Routes>
+                    <Route excat path="/" element={<MainPage/>} />
                     <Route excat path="/begin" element={<Beginner/>} />
                     <Route excat path="/result" element={<Result/>} />
                 </Routes>
