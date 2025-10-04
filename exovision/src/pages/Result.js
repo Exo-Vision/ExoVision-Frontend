a06811eec9c9827eb9ff66a71ca6b420558349fa
Lihan <lihan3k@gmail.com>
2025-10-04 21:46:54 +0900

    Feat: 그래프 틀 구현 및 Result.js 컴포넌트 버튼 구현

--- exovision/src/pages/Result.js
+++ exovision/src/pages/Result.js
@@ -1,7 +1,10 @@
 import './Result.css'
 import exovision from './../assets/EXOVISION.png'
+import { useNavigate } from 'react-router-dom'
 
 function Result() {
+    const navigate = useNavigate();
+
     return (
         <div className='ResultBody'>
             <div className='ResultHeader'><img src={exovision}></img></div>
@@ -48,19 +51,78 @@ function Result() {
                     <div className='Result3DetailValue'>-<br/>-<br/>-<br/>-<br/>-</div>
                 </div>
             </div>
+            <div className='ResultPOGraphTitle'>Predicted Orbit Graph</div>
             <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className='ResultHalfGraphForm'>
+                <div className='ResultROCGraph'>
+                    <div className='ResultHalfGraphTitle'>ROC Curve</div>
+                    <div></div>
+                </div>
+                <div className='ResultTLCGraph'>
+                    <div className='ResultHalfGraphTitle'>Transit Light Curve</div>
+                    <div></div>
+                </div>
+            </div>
+            <div className='ResultFPRGraphForm'>
+                <div className='ResultFPRGraphBox'>
+                    <div className='ResultFPRGraphTitle'>F1 Score</div>
+                    <div></div>
+                </div>
+                <div className='ResultFPRGraphBox'>
+                    <div className='ResultFPRGraphTitle'>Precision</div>
+                    <div></div>
+                </div>
+                <div className='ResultFPRGraphBox'>
+                    <div className='ResultFPRGraphTitle'>Recall</div>
+                    <div></div>
+                </div>
+            </div>
+            <div className='ResultSelectForm'>
+                <div className='ResultSelectTitle'>Select Missions</div>
+                <div className='ResultSelectBox'>
+                    <input type='checkbox' className='ResultSelectInput'></input>
+                    <div className='ResultSelectInputText'>K2</div>
+                    <div className='ResultSelectInputK2'>7</div>
+                    <input type='checkbox' className='ResultSelectInput'></input>
+                    <div className='ResultSelectInputText'>Kepler</div>
+                    <div className='ResultSelectInputKepler'>7</div>
+                    <input type='checkbox' className='ResultSelectInput'>
+                    </input><div className='ResultSelectInputText'>TESS</div>
+                    <div className='ResultSelectInputTESS'>6</div>
+                </div>
+            </div>
+            <div className='ResultPRDGraphForm'>
+                <div className='ResultExpertGraphTitle'>Planet Radius Distribution</div>
+                <div></div>
+            </div>
+            <div className='ResultExpertGraphForm'>
+                <div className='ResultExpertGraphTitle'>Orbital Period vs Planet Radius</div>
+                <div></div>
+            </div>
+            <div className='ResultExpertGraphForm'>
+                <div className='ResultExpertGraphTitle'>Orbital Period vs Transit Depth</div>
+                <div></div>
+            </div>
+            <div className='ResultExpertGraphForm'>
+                <div className='ResultExpertGraphTitle'>Transit Duration Distribution</div>
+                <div></div>
+            </div>
+            <div className='ResultTDDRowGraphForm'>
+                <div className='ResultExpertGraphTitle'>Transit Duration Distribution</div>
+                <div></div>
+            </div>
+            <div className='ResultTTDSquareGraphForm'>
+                <div className='ResultExpertGraphTitle'>Transit Duration Distribution</div>
+                <div></div>
+            </div>
+            <div className='ResultSaveForm2'>
+                <div className='ResultSave2'><div className='ResultSaveText2'>+Log This Planet</div></div>
+                <div className='ResultDiscovery2'><div className='ResultDiscoveryText2'>Discovery Log</div></div>
+            </div>
+            <div className='ResultButtonForm'>
+                <div className='ResultRefine'><div className='ResultRefineText'>Refine Input</div></div>
+                <div className='ResultRestart' onClick={() => navigate("/")}><div className='ResultRestartText2'>Restart Mission</div></div>
+            </div>
         </div>
     );
 }
