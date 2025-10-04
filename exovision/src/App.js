1b5635607b10ad22292d28926c4665a62306649c
Lihan <lihan3k@gmail.com>
2025-10-04 12:27:53 +0900

    Create: Expert.js, Expert.css 파일 생성 및 컴포넌트개발

--- exovision/src/App.js
+++ exovision/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Expert from './pages/Expert'
+
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    
+                    <Route excat path="/expert" element={<Expert/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
