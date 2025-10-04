08da0961086b68453768bab2e8e6bc382d7663f9
Lihan <96524080+Lihan00@users.noreply.github.com>
2025-10-04 12:31:01 +0900

    Revert "Create: Expert.js, Expert.css 파일 생성 및 컴포넌트개발"

--- exovision/src/App.js
+++ exovision/src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Expert from './pages/Expert'
-
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route excat path="/expert" element={<Expert/>}/>
+                    
                 </Routes>
             </BrowserRouter>
         </div>
